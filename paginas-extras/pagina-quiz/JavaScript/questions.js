f73f26d2551e5bbaa24b108adc49fc1463b6a6bf
Jonas Araújo <135404598+sraraujo@users.noreply.github.com>
2023-09-11 16:22:56 -0300

    últimos ajustes

---
+++ paginas-extras/pagina-quiz/JavaScript/questions.js
@@ -0,0 +1,111 @@
+export default [
+  {
+    question: "Qual a capital da Jamaica?",
+    answers: [
+      { option: "Brasília", correct: false },
+      { option: "Paris", correct: false },
+      { option: "Kingston", correct: true },
+      { option: "Roma", correct: false },
+      { option: "Madrid", correct: false },
+    ],
+  },
+
+  {
+    question: "Em qual continente está localizada a Jamaica?",
+    answers: [
+      { option: "Europa", correct: false },
+      { option: "África", correct: false },
+      { option: "Ásia", correct: false },
+      { option: "América Central", correct: true },
+      { option: "Oceania", correct: false },
+    ],
+  },
+
+  {
+    question: "Qual o idioma oficial da jamaica?",
+    answers: [
+      { option: "Português", correct: false },
+      { option: "Mandarim", correct: false },
+      { option: "Inglês", correct: true },
+      { option: "Castelhano (castellano)", correct: false },
+      { option: "Francês", correct: false },
+    ],
+  },
+
+  {
+    question: "Qual a moeda monetária da Jamaica?",
+    answers: [
+      { option: "Real", correct: false },
+      { option: "Dólar Jamaicano", correct: true },
+      { option: "Libra", correct: false },
+      { option: "Yuan chinês", correct: false },
+      { option: "Peso Argentino", correct: false },
+    ],
+  },
+
+  {
+    question: "Qual as cores da bandeira da Jamaica?",
+    answers: [
+      { option: "Azul, branco e vermelho", correct: false },
+      { option: "Amarelo, verde e azul", correct: false },
+      { option: "Verde, preto e amarelo", correct: true },
+      { option: "Verde, preto e branco", correct: false },
+      { option: "Preto, amarelo e azul", correct: false },
+    ],
+  },
+
+  {
+    question: "Qual país faz fronteira com a Jamaica?",
+    answers: [
+      { option: "EUA e México", correct: false },
+      { option: "Espanha e França ", correct: false },
+      { option: "China e Índia", correct: false },
+      { option: "Cuba e Haiti", correct: false },
+      { option: "Nenhum", correct: true },
+    ],
+  },
+
+  {
+    question: "Qual a religião predominante na Jamaica?",
+    answers: [
+      { option: "Candomblé", correct: false },
+      { option: "Cristianismo", correct: true },
+      { option: "Espirita", correct: false },
+      { option: "Ateísmo", correct: false },
+      { option: "Hinduísmo", correct: false },
+    ],
+  },
+  
+  {
+    question: "Qual a forma de governo da Jamaica?",
+    answers: [
+      { option: "Presidencialismo", correct: false },
+      { option: "Semipresidencialista ", correct: false },
+      { option: "Monarquia Parlamentarista", correct: true },
+      { option: "Parlamentarista", correct: false },
+      { option: "Monarquia", correct: false },
+    ],
+  },
+
+  {
+    question: "Qual o nome do primeiro-minsitro da Jamaica?",
+    answers: [
+      { option: "Joe Biden", correct: false },
+      { option: "Andrew Holness", correct: true },
+      { option: "Emmanuel Macron", correct: false },
+      { option: "Tayyip Erdogan", correct: false },
+      { option: "Nayib Bukele", correct: false },
+    ],
+  },
+
+  {
+    question: "Qual o clima predominante da Jamaica?",
+    answers: [
+      { option: "Equatorial", correct: false },
+      { option: "Temperado", correct: false },
+      { option: "Tropical", correct: true },
+      { option: "Subtropical", correct: false },
+      { option: "Árido", correct: false },
+    ],
+  }
+];
